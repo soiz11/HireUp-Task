08cc932ee6392d0b4f49dc38e520b13473c4436e
Sohan Prabhath <soizsinghe@gmail.com>
2024-08-18 03:34:33 +0530

    90% over

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,6 +1,7 @@
 "use client";
 import { IoIosArrowDown } from "react-icons/io";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { IoIosSearch } from "react-icons/io";
 
 //all the click handlesr should issolated to seperate client components
 //but in here i place all the components and others in same file
@@ -28,23 +29,60 @@ const faqs = [
 const Faq = () => {
   const [expandedIndex, setExpandedIndex] = useState(null);
 
+  const [inputValue, setInputValue] = useState("");
+
   const handleToggle = (index) => {
     setExpandedIndex(expandedIndex === index ? null : index);
   };
 
+  const HandleToggleAll = () => {};
+
+  const inputRef = useRef(null);
+
+  const filterdArray = faqs.filter((q) => q.question.startsWith(inputValue));
+  console.log(filterdArray);
+
+  const selectedArray = inputValue == "" ? faqs : filterdArray;
+
   return (
-    <div className="flex flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
+    <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
+      <div className="flex  top-[10%] 2xl:right-[30%] right-[5%] 2xl:left-[30%] left-[5%] absolute md:px-5 px-3 2xl:py-2 py-1 bg-white rounded-md shadow-xl">
+        <input
+          type="text"
+          className="flex-1 outline-none md:text-[18px] text-[14px] text-indigo-500 font-semibold placeholder:md:text-[18px] placeholder:text-[14px] placeholder:font-normal placeholder:text-indigo-500"
+          onChange={(e) => setInputValue(e.target.value)}
+          ref={inputRef}
+          placeholder="Search Your Problem"
+        />
+        <IoIosSearch
+          className="text-indigo-500 2xl:size-[30px] size-[25px]"
+          onClick={() => inputRef.current.focus()}
+        />
+      </div>
+
       <div className="text-[70px] font-semibold">FAQ</div>
+
       <div className="flex flex-col 2xl:w-[60%] w-[90%] gap-y-5">
-        {faqs.map((qu, index) => (
+        {selectedArray.map((qu, index) => (
           <QuizBox
             key={index}
             quiz={qu.question}
             answer={qu.answer}
-            isExpanded={expandedIndex === index}
+            isExpanded={expandedIndex == index}
             onToggle={() => handleToggle(index)}
           />
         ))}
+        {selectedArray.length < 1 && (
+          <div className="md:text-[18px] text-[14px]   py-2 text-center">
+            Sorry, no matches found. Please try a different search term.
+          </div>
+        )}
+      </div>
+      <div
+        className="absolute bottom-[10%] right-[10%]"
+        onClick={HandleToggleAll}
+      >
+        all
       </div>
     </div>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
 import Link from "next/link";
 
 export default function Home() {
-  return <Link href="/faq">To FAQ</Link>;
+  return (
+    <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
+      <Link
+        href="/faq"
+        className="px-5 py-2 bg-indigo-500 text-white 2xl:text-[18px] text-[14px] rounded-md shadow-lg"
+      >
+        To FAQ Page
+      </Link>
+    </div>
+  );
 }
