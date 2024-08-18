ea278530b2b101ef9df2c3b13fc360ca794e2ce7
Sohan Prabhath <soizsinghe@gmail.com>
2024-08-18 16:10:55 +0530

    re arrangement

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -27,20 +27,29 @@ const faqs = [
   },
 ];
 
-//main component
-
 const Faq = () => {
+  return (
+    <Suspense
+      fallback={
+        <div className="px-5 py-2 text-indigo-500 bg-white rounded-md">
+          Loading...
+        </div>
+      }
+    >
+      <FaqContent />
+    </Suspense>
+  );
+};
+
+//main component
+const FaqContent = () => {
   const [expandedIndices, setExpandedIndices] = useState([]);
 
   const searchParams = useSearchParams();
   const router = useRouter();
   const search = searchParams.get("search") || "";
-  console.log(search);
-
-  //in here im not using debounce technique to search in case of instant search
   const [inputValue, setInputValue] = useState(search);
-  console.log(inputValue);
-
+  //in searching im not using debounce principle incase of get instant search apperance
   const handleToggle = (index) => {
     setExpandedIndices((prev) =>
       prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]
@@ -61,68 +70,58 @@ const Faq = () => {
 
   const inputRef = useRef(null);
 
-  const filterdArray = faqs.filter((q) => q.question.startsWith(inputValue));
-  console.log(filterdArray);
+  const filteredArray = faqs.filter((q) => q.question.startsWith(inputValue));
+  const selectedArray = inputValue === "" ? faqs : filteredArray;
 
-  const selectedArray = inputValue == "" ? faqs : filterdArray;
-
-  console.log(expandedIndices.length);
   return (
-    <Suspense
-      fallback={
-        <div className="text-indigo-500 bg-white px-5 py-2 rounded-md">
-          Loading...
-        </div>
-      }
-    >
-      <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
-        <div className="flex  top-[10%] 2xl:right-[30%] right-[5%] 2xl:left-[30%] left-[5%] absolute md:px-5 px-3 2xl:py-2 py-1 bg-white rounded-md shadow-xl">
-          <input
-            type="text"
-            className="flex-1 outline-none md:text-[18px] text-[14px] text-indigo-500 font-semibold placeholder:md:text-[18px] placeholder:text-[14px] placeholder:font-normal placeholder:text-indigo-500"
-            onChange={(e) => setInputValue(e.target.value)}
-            ref={inputRef}
-            placeholder="Search Your Problem"
-            value={inputValue}
+    <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
+      <div className="flex  top-[10%] 2xl:right-[30%] right-[5%] 2xl:left-[30%] left-[5%] absolute md:px-5 px-3 2xl:py-2 py-1 bg-white rounded-md shadow-xl">
+        <input
+          type="text"
+          className="flex-1 outline-none md:text-[18px] text-[14px] text-indigo-500 font-semibold placeholder:md:text-[18px] placeholder:text-[14px] placeholder:font-normal placeholder:text-indigo-500"
+          onChange={(e) => setInputValue(e.target.value)}
+          ref={inputRef}
+          placeholder="Search Your Problem"
+          value={inputValue}
+        />
+        <Link href={`?search=${inputValue}`}>
+          <IoIosSearch
+            className="text-indigo-500 2xl:size-[30px] size-[25px]"
+            onClick={() => inputRef.current.focus()}
           />
-          <Link href={`?search=${inputValue}`}>
-            <IoIosSearch
-              className="text-indigo-500 2xl:size-[30px] size-[25px]"
-              onClick={() => inputRef.current.focus()}
-            />
-          </Link>
-        </div>
-
-        <div className="text-[70px] font-semibold">FAQ</div>
-
-        <div className="flex flex-col 2xl:w-[60%] w-[90%] gap-y-5">
-          {selectedArray.map((qu, index) => (
-            <QuizBox
-              key={index}
-              quiz={qu.question}
-              answer={qu.answer}
-              isExpanded={expandedIndices.includes(index)}
-              onToggle={() => handleToggle(index)}
-            />
-          ))}
-          {selectedArray.length < 1 && (
-            <div className="md:text-[18px] text-[14px]   py-2 text-center">
-              Sorry, no matches found. Please try a different search term.
-            </div>
-          )}
-        </div>
-        <div
-          className="absolute bottom-[15%] 2xl:right-[30%] right-[5%] bg-white rounded-md p-2 shadow-custom size-[50px] flex items-center justify-center"
-          onClick={HandleToggleAll}
-        >
-          <GoCircle
-            className={`text-indigo-500 rounded-full transition-all duration-500 ${
-              expandedIndices.length === 0 ? "text-[35px]" : "text-[25px]"
-            }`}
-          />
-        </div>
+        </Link>
       </div>
-    </Suspense>
+
+      <div className="text-[70px] font-semibold">FAQ</div>
+
+      <div className="flex flex-col 2xl:w-[60%] w-[90%] gap-y-5">
+        {selectedArray.map((qu, index) => (
+          <QuizBox
+            key={index}
+            quiz={qu.question}
+            answer={qu.answer}
+            isExpanded={expandedIndices.includes(index)}
+            onToggle={() => handleToggle(index)}
+          />
+        ))}
+        {selectedArray.length < 1 && (
+          <div className="md:text-[18px] text-[14px] py-2 text-center">
+            Sorry, no matches found. Please try a different search term.
+          </div>
+        )}
+      </div>
+
+      <div
+        className="absolute bottom-[15%] 2xl:right-[20%] right-[5%] bg-white rounded-md p-2 shadow-custom size-[50px] flex items-center justify-center"
+        onClick={HandleToggleAll}
+      >
+        <GoCircle
+          className={`text-indigo-500 rounded-full transition-all duration-500 ${
+            expandedIndices.length === 0 ? "text-[35px]" : "text-[25px]"
+          }`}
+        />
+      </div>
+    </div>
   );
 };
 
@@ -131,37 +130,29 @@ export default Faq;
 // QuizBox Component
 const QuizBox = ({ quiz, answer, isExpanded, onToggle }) => {
   return (
-    <Suspense
-      fallback={
-        <div className="text-indigo-500 bg-white px-5 py-2 rounded-md">
-          Loading...
-        </div>
-      }
-    >
-      <div className="flex flex-col w-full shadow-md">
-        <div
+    <div className="flex flex-col w-full shadow-md">
+      <div
+        className={`${
+          isExpanded ? "rounded-t-md" : "rounded-md"
+        } md:px-5 px-3 py-2 flex justify-between items-center bg-white`}
+        onClick={onToggle}
+      >
+        <div className="font-semibold md:text-[18px] text-[14px]">{quiz}</div>
+        <IoIosArrowDown
           className={`${
-            isExpanded ? "rounded-t-md" : "rounded-md"
-          } md:px-5 px-3 py-2 flex justify-between items-center bg-white`}
-          onClick={onToggle}
-        >
-          <div className="font-semibold md:text-[18px] text-[14px]">{quiz}</div>
-          <IoIosArrowDown
-            className={`${
-              isExpanded ? "rotate-180" : "rotate-0"
-            } duration-500 transition-all w-[20px]`}
-          />
-        </div>
-        <div
-          className={`${
-            isExpanded
-              ? "h-auto opacity-100 py-2 border-t-2 border-indigo-300"
-              : "h-0 opacity-0"
-          } w-full bg-white text-gray-700 font-light transition-all duration-500 rounded-b-md px-5 overflow-hidden md:text-[16px] text-[12px]`}
-        >
-          {answer}
-        </div>
+            isExpanded ? "rotate-180" : "rotate-0"
+          } duration-500 transition-all w-[20px]`}
+        />
       </div>
-    </Suspense>
+      <div
+        className={`${
+          isExpanded
+            ? "h-auto opacity-100 py-2 border-t-2 border-indigo-300"
+            : "h-0 opacity-0"
+        } w-full bg-white text-gray-700 font-light transition-all duration-500 rounded-b-md px-5 overflow-hidden md:text-[16px] text-[12px]`}
+      >
+        {answer}
+      </div>
+    </div>
   );
 };
