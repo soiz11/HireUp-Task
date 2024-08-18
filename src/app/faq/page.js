ad3cf292695c020b1b2bb1767ba71142c8c810f8
Sohan Prabhath <soizsinghe@gmail.com>
2024-08-18 15:52:04 +0530

    wrapped with suspense boundry

--- src/app/faq/page.js
+++ src/app/faq/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { IoIosArrowDown } from "react-icons/io";
-import React, { useEffect, useRef, useState } from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
@@ -68,53 +68,61 @@ const Faq = () => {
 
   console.log(expandedIndices.length);
   return (
-    <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
-      <div className="flex  top-[10%] 2xl:right-[30%] right-[5%] 2xl:left-[30%] left-[5%] absolute md:px-5 px-3 2xl:py-2 py-1 bg-white rounded-md shadow-xl">
-        <input
-          type="text"
-          className="flex-1 outline-none md:text-[18px] text-[14px] text-indigo-500 font-semibold placeholder:md:text-[18px] placeholder:text-[14px] placeholder:font-normal placeholder:text-indigo-500"
-          onChange={(e) => setInputValue(e.target.value)}
-          ref={inputRef}
-          placeholder="Search Your Problem"
-          value={inputValue}
-        />
-        <Link href={`?search=${inputValue}`}>
-          <IoIosSearch
-            className="text-indigo-500 2xl:size-[30px] size-[25px]"
-            onClick={() => inputRef.current.focus()}
+    <Suspense
+      fallback={
+        <div className="text-indigo-500 bg-white px-5 py-2 rounded-md">
+          Loading...
+        </div>
+      }
+    >
+      <div className="flex relative flex-col gap-y-8 items-center justify-center w-full min-h-screen bg-gradient-radial from-white to-indigo-500">
+        <div className="flex  top-[10%] 2xl:right-[30%] right-[5%] 2xl:left-[30%] left-[5%] absolute md:px-5 px-3 2xl:py-2 py-1 bg-white rounded-md shadow-xl">
+          <input
+            type="text"
+            className="flex-1 outline-none md:text-[18px] text-[14px] text-indigo-500 font-semibold placeholder:md:text-[18px] placeholder:text-[14px] placeholder:font-normal placeholder:text-indigo-500"
+            onChange={(e) => setInputValue(e.target.value)}
+            ref={inputRef}
+            placeholder="Search Your Problem"
+            value={inputValue}
           />
-        </Link>
-      </div>
+          <Link href={`?search=${inputValue}`}>
+            <IoIosSearch
+              className="text-indigo-500 2xl:size-[30px] size-[25px]"
+              onClick={() => inputRef.current.focus()}
+            />
+          </Link>
+        </div>
 
-      <div className="text-[70px] font-semibold">FAQ</div>
+        <div className="text-[70px] font-semibold">FAQ</div>
 
-      <div className="flex flex-col 2xl:w-[60%] w-[90%] gap-y-5">
-        {selectedArray.map((qu, index) => (
-          <QuizBox
-            key={index}
-            quiz={qu.question}
-            answer={qu.answer}
-            isExpanded={expandedIndices.includes(index)}
-            onToggle={() => handleToggle(index)}
+        <div className="flex flex-col 2xl:w-[60%] w-[90%] gap-y-5">
+          {selectedArray.map((qu, index) => (
+            <QuizBox
+              key={index}
+              quiz={qu.question}
+              answer={qu.answer}
+              isExpanded={expandedIndices.includes(index)}
+              onToggle={() => handleToggle(index)}
+            />
+          ))}
+          {selectedArray.length < 1 && (
+            <div className="md:text-[18px] text-[14px]   py-2 text-center">
+              Sorry, no matches found. Please try a different search term.
+            </div>
+          )}
+        </div>
+        <div
+          className="absolute bottom-[15%] 2xl:right-[30%] right-[5%] bg-white rounded-md p-2 shadow-custom size-[50px] flex items-center justify-center"
+          onClick={HandleToggleAll}
+        >
+          <GoCircle
+            className={`text-indigo-500 rounded-full transition-all duration-500 ${
+              expandedIndices.length === 0 ? "text-[35px]" : "text-[25px]"
+            }`}
           />
-        ))}
-        {selectedArray.length < 1 && (
-          <div className="md:text-[18px] text-[14px]   py-2 text-center">
-            Sorry, no matches found. Please try a different search term.
-          </div>
-        )}
+        </div>
       </div>
-      <div
-        className="absolute bottom-[15%] 2xl:right-[30%] right-[5%] bg-white rounded-md p-2 shadow-custom size-[50px] flex items-center justify-center"
-        onClick={HandleToggleAll}
-      >
-        <GoCircle
-          className={`text-indigo-500 rounded-full transition-all duration-500 ${
-            expandedIndices.length === 0 ? "text-[35px]" : "text-[25px]"
-          }`}
-        />
-      </div>
-    </div>
+    </Suspense>
   );
 };
 
